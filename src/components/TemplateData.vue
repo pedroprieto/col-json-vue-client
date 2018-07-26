699baa66b98e20c3c51a368eb6df3c23cc75d361
Pedro Prieto <pedpral@gmail.com>
2018-07-26 17:33:53 +0200

    Fix form validation before put or post template. Add support for data.type='file' and attachments in multipart/form-data format.

--- src/components/TemplateData.vue
+++ src/components/TemplateData.vue
@@ -1,24 +1,26 @@
 <template>
 
-<form class="box">
+<form class="box" @submit="createOrUpdate($event)">
   <div v-for="data in editTemplate.data" class="field">
     <label class="label" :for="data.name">{{data.prompt}}</label>
     <div class="control" v-model="data.value">
-      <select v-if="data.type == 'select'" class="input" name="" :id="data.name" v-model="data.value">
+      <select v-if="data.type == 'select'" class="input" name="" :id="data.name" v-model="data.value" :required="data.required">
         <option v-for="op in collection.related[data.suggest.related]" :value="op[data.suggest.value]">
           {{op[data.suggest.text]}}
         </option>
       </select>
-      <textarea v-else-if="data.type == 'textarea'" class="input" name="data.name" :type="data.type" :id="data.name" v-model="data.value">
+      <textarea v-else-if="data.type == 'textarea'" class="input" name="data.name" :type="data.type" :id="data.name" v-model="data.value" :required="data.required">
       </textarea>
       <div v-else-if="data.type == 'notification'" v-html="data.value" class="notification is-info" name="data.name" :id="data.name">
       </div>
-      <input v-else class="input" name="data.name" :type="data.type" :id="data.name" v-model="data.value">
+      <!-- <FileComponent v-else-if="data.type == 'file'" :data="data"></FileComponent> -->
+      <input v-else-if="data.type == 'file'" multiple class="input" name="data.name" :id="data.name" type="file" @change="updateFile($event, data)" :required="data.required">
+      <input v-else class="input" name="data.name" :type="data.type" :id="data.name" v-model="data.value" :required="data.required">
     </div>
   </div>
   
   <div class="field has-text-centered">
-    <input type="submit" class="button" @click="createOrUpdate($event)"></input>
+    <input type="submit" class="button"></input>
   </div>
 </form>
 
@@ -44,19 +46,19 @@ export default {
       var e = {};
       e.data = [];
       for (var field of this.collection.template.data) {
+        var f = {
+          name: field.name,
+          prompt: field.prompt,
+          type: field.type,
+          value: '',
+          suggest: field.suggest
+        }
         for (var d of this.item.data) {
           if (d.name == field.name) {
-            e.data.push(
-              {
-                name: field.name,
-                prompt: field.prompt,
-                value: d.value,
-                type: field.type,
-                suggest: field.suggest
-              }
-            )
+            f.value = d.value;
           }
         }
+        e.data.push(f);
       }
       return e;
     }
@@ -68,6 +70,10 @@ export default {
   components: {
   },
   methods: {
+    updateFile: function(e, data) {
+      // Store in data.value the file list of the input='file'
+      data.value = e.target.files;
+    },
     createOrUpdate: function(e) {
       e.preventDefault();
       if (this.item) {
@@ -78,6 +84,33 @@ export default {
       }
       
     },
+    buildSendData: function() {
+      // Add support for attachments
+      // https://github.com/collection-json/extensions/pull/7
+      
+      // Default: send data in CJ format
+      var dataToSend = {template: this.editTemplate};
+      
+      if ( (this.collection.template.contentType) && (this.collection.template.contentType == "multipart/form-data") ) {
+        // If template.contentType exists and is 'multipart/form-data', send the data in Form-Data format. Also, include attachments
+        var form_data = new FormData();
+        
+        // Transform CJ template to FormData
+        for ( var key of this.editTemplate.data) {
+          // Check if file component has more than one file selected
+          if (key.value instanceof FileList) {
+            for (var i of key.value) {
+              form_data.append(key.name, i);
+            }
+          } else {
+            form_data.append(key.name, key.value);
+          }
+        }
+        dataToSend = form_data;
+      };
+      
+      return dataToSend;
+    },
     createItem: function() {
       // Method to do a POST request to create an item in a collection
 	    // The POST request must be sent to the collection.href property of the collection object
@@ -87,7 +120,7 @@ export default {
       
       // Post to the collection url, available in this.createurl
       // We send the 'template' prop, which is bound to the form
-      axios.post(this.collection.href, {template: this.editTemplate})
+      axios.post(this.collection.href, this.buildSendData())
         .then(function (response) {
           // Emit an event to read again the collection
           // The App component will listen to the 'refresh' event and it will call the readCollection method
@@ -108,7 +141,7 @@ export default {
 	    // It must send the TEMPLATE object of the collection with the data filled by the user
 	    // http://amundsen.com/media-types/collection/examples/#ex-write
       
-      axios.put(this.item.href, {template: this.editTemplate})
+      axios.put(this.item.href, this.buildSendData())
         .then(function (response) {
           // Emit an event to read again the collection
           // The App component will listen to the 'refresh' event and it will call the readCollection method
